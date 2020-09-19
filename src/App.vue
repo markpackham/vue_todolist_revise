f14dc9dcdceb44677884d0c3eca86a4b1f47b1fa
mark <markpackham1@gmail.com>
2020-09-19 12:28:10 +0100

    add todo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos :todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 export default {
   name: "App",
   components: {
-    Todos,
     Header,
+    Todos,
+    AddTodo,
   },
   data() {
     return {
@@ -39,6 +42,9 @@ export default {
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
     },
   },
 };
